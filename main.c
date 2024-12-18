4ef3ed545a070806a75110ee7afb2c9421e07d49
Lidreyx <127121808+Lidreyx@users.noreply.github.com>
2024-12-18 11:38:05 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "morse.c"
+#include "mqtt.c"
+#include "sleep.c"
+#include "led.c"
+
+
+
+int main(){
+
+}
+
+on_mqtt_msg(){
+
+}
+
+on_morse_ev(enum){
+    
+}
