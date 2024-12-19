4c3cd6e148fda76e2760058ac2002196993dc2ef
Lidreyx <127121808+Lidreyx@users.noreply.github.com>
2024-12-19 09:26:54 +0100

    Update led.c

--- led.c
+++ led.c
@@ -1,21 +1,26 @@
+#include <wiringPi.h>
 #include <stdio.h>
+#include <string.h>
 
-int ledPin= ; //the pin where the led is connected
+#define LED_PIN 7  // GPIO 7 pour cet exemple
 
-void setup()
-{
-    pinMode(ledPin, OUTPUT); //sets digital as output
+// Fonction pour envoyer un signal Morse à la LED
+void MorseToLed(const char* morse) {
+    for (int i = 0; morse[i] != '\0'; i++) {
+        if (morse[i] == '-') {  // Si c'est un tiret, signal long
+            digitalWrite(LED_PIN, HIGH);  // Allume la LED
+            delay(1000);  // 1 seconde
+            digitalWrite(LED_PIN, LOW);  // Éteint la LED
+            delay(500);  // Pause de 500ms
+        } else if (morse[i] == '.') {  // Si c'est un point, signal court
+            digitalWrite(LED_PIN, HIGH);  // Allume la LED
+            delay(500);  // 500ms
+            digitalWrite(LED_PIN, LOW);  // Éteint la LED
+            delay(500);  // Pause de 500ms
+        } else if (morse[i] == '/') {  // Espace entre les lettres
+            digitalWrite(LED_PIN, LOW);  // Éteint la LED
+            delay(1000);  // Pause de 1 seconde
+        }
+    }
 }
 
-void loop()
-{
-    digitalWrite(ledPin, HIGH);//Led on
-    delay(1000);
-    digitalWrite(ledPin, LOW);//Led off
-    delay(1000);
-}
-
-led_new(const char* name) : led_t*
-
-led_light_short(led_t)
-led_light_long(led_t)
