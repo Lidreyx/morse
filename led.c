a57c957fa881bd841a07ac618d6d24e4c808f7aa
Lidreyx <127121808+Lidreyx@users.noreply.github.com>
2024-12-19 17:09:36 +0100

    Update led.c

--- led.c
+++ led.c
@@ -4,22 +4,22 @@
 
 #define LED_PIN 7  // GPIO 7 pour cet exemple
 
-// Fonction pour envoyer un signal Morse à la LED
+// Function to change morse into led signal
 void MorseToLed(const char* morse) {
     for (int i = 0; morse[i] != '\0'; i++) {
-        if (morse[i] == '-') {  // Si c'est un tiret, signal long
-            digitalWrite(LED_PIN, HIGH);  // Allume la LED
+        if (morse[i] == '-') {  // if dash, long signal
+            digitalWrite(LED_PIN, HIGH);  // turn on  LED
             delay(1000);  // 1 seconde
-            digitalWrite(LED_PIN, LOW);  // Éteint la LED
-            delay(500);  // Pause de 500ms
-        } else if (morse[i] == '.') {  // Si c'est un point, signal court
-            digitalWrite(LED_PIN, HIGH);  // Allume la LED
+            digitalWrite(LED_PIN, LOW);  // turn off LED
+            delay(500);  // delay 500ms
+        } else if (morse[i] == '.') {  // if dot, long signal
+            digitalWrite(LED_PIN, HIGH);  // turn on LED
             delay(500);  // 500ms
-            digitalWrite(LED_PIN, LOW);  // Éteint la LED
+            digitalWrite(LED_PIN, LOW);  // turn off LED
             delay(500);  // Pause de 500ms
-        } else if (morse[i] == '/') {  // Espace entre les lettres
-            digitalWrite(LED_PIN, LOW);  // Éteint la LED
-            delay(1000);  // Pause de 1 seconde
+        } else if (morse[i] == '/') {  // space between word
+            digitalWrite(LED_PIN, LOW);  // turn off LED
+            delay(1000);  // 1s delay if space
         }
     }
 }
